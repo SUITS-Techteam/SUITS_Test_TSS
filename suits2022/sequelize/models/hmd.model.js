5361d1321cc32dad8c72df1e5723b3d1d3faef59
Faiz Khan <faiz.a.khan@nasa.gov>
2022-11-28 08:41:49 -0600

    Add new models for hardware

---
+++ suits2022/sequelize/models/hmd.model.js
@@ -0,0 +1,29 @@
+const { DataTypes } = require('sequelize');
+
+// We export a function that defines the model.
+// This function will automatically receive as parameter the Sequelize connection object.
+module.exports = (sequelize) => {
+	sequelize.define('hmd', {
+		// The following specification of the 'id' attribute could be omitted
+		// since it is the default.
+		id: {
+			allowNull: false,
+			autoIncrement: true,
+			primaryKey: true,
+			type: DataTypes.INTEGER
+		},
+		name: {
+			allowNull: false,
+			type: DataTypes.STRING,
+			unique: true
+		},
+		description: {
+			allowNull: true,
+			type: DataTypes.STRING
+		},
+		assignment: {
+			allowNull: true,
+			type: DataTypes.STRING
+		}
+	});
+};
