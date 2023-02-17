e68e8b3898251292cb502707e42bad25989e0509
Cisneros <gacisner@ndc.nasa.gov>
2023-02-16 20:53:42 -0600

    edits server for pushing data without registration

---
+++ suits2022/sockets/clients/test_client.js
@@ -0,0 +1,15 @@
+const SocketServer = require('ws');
+const ws = new SocketServer.WebSocket('ws://localhost:3001');
+const fs = require("fs");
+const pth = require("path");
+
+room = 1;
+
+ws.on('message', (d) => {
+	console.log("opened");
+	console.log(ws.readyState)
+	console.log(d.toString())
+
+	let msg = {"MSGTYPE": "DATA", "BLOB": { DATATYPE: "STUDENT", ROOM: room, DATA: null}};
+	ws.send(JSON.stringify(msg));
+});
